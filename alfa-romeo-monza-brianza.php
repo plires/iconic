cab4e17429f9adfc2fcb1f84c296f3aebba4a6b5
Pablo Lires <pablolires@gmail.com>
2022-04-25 10:46:11 -0300

    Rename de pages

--- alfa-romeo-monza-brianza.php
+++ alfa-romeo-monza-brianza.php
@@ -6,8 +6,8 @@
   
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Hand built full Aluminum body 1952 Alfa Romeo 1900 C52 Disco Volante Fianchi Stretti.">
-  <title>Iconic Auto Sports. - Alfa Romeo Fianchi Stretti</title>
+  <meta name="description" content="1933 Alfa Romeo 8C Monza Brianza. A recreation of the highest levels completed two decades ago commission by a very well known collector">
+  <title>Iconic Auto Sports. - Alfa Romeo Monza brianza</title>
 
   <!-- Favicons -->
   <?php include('includes/favicon.php'); ?>
@@ -20,7 +20,7 @@
 </head>
 <body>
   
-  <?php $current = 'fianchi'; ?>
+  <?php $current = 'monza'; ?>
 
   <!-- Modal -->
   <?php include('includes/contacto-modal.php'); ?>
@@ -42,34 +42,34 @@
           <div class="row">
             
             <div class="col-lg-6 carusel">
-              <div id="carouselfianchiControls" class="carousel slide" data-bs-ride="carousel">
+              <div id="carouselMonzaControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
 
                   <div class="carousel-item active">
-                    <img src="./img/alfa-1900-slide-1.jpg" class="d-block w-100" alt="slide 1 alfa romeo">
+                    <img src="./img/monza-slide-1.jpg" class="d-block w-100" alt="slide 1 alfa romeo monza stretti">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-2.jpg" class="d-block w-100" alt="slide 2 alfa romeo">
+                    <img src="./img/monza-slide-2.jpg" class="d-block w-100" alt="slide 2 alfa romeo monza stretti">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-3.jpg" class="d-block w-100" alt="slide 3 alfa romeo">
+                    <img src="./img/monza-slide-3.jpg" class="d-block w-100" alt="slide 3 alfa romeo monza stretti">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-4.jpg" class="d-block w-100" alt="slide 4 alfa romeo">
+                    <img src="./img/monza-slide-4.jpg" class="d-block w-100" alt="slide 4 alfa romeo monza stretti">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-5.jpg" class="d-block w-100" alt="slide 5 alfa romeo">
+                    <img src="./img/monza-slide-5.jpg" class="d-block w-100" alt="slide 5 alfa romeo monza stretti">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-6.jpg" class="d-block w-100" alt="slide 6 alfa romeo">
+                    <img src="./img/monza-slide-6.jpg" class="d-block w-100" alt="slide 6 alfa romeo monza stretti">
                   </div>
 
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselfianchiControls" data-bs-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselMonzaControls" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselfianchiControls" data-bs-slide="next">
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselMonzaControls" data-bs-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Next</span>
                 </button>
@@ -79,12 +79,10 @@
             <div class="col-lg-6 data">
               <ul>
                 <li><h2 class="title">available</h2></li>
-                <li><p class="modelo">1952 alfa romeo<br><span>1900 c52</span></p></li>
-                <li><p class="caracteristicas">ENGINE SIZE<br><span>1600</span></p></li>
-                <li><p class="caracteristicas">BODY STYLE<br><span>roadster</span></p></li>
-                <li><p class="caracteristicas">MILES<br><span>3,009</span></p></li>
-                <li><p class="caracteristicas">VIN<br><span>OHA005FS18</span></p></li>
-                <li><p class="caracteristicas">STOCK<br><span>FS18</span></p></li>
+                <li><p class="modelo">1933 alfa romeo<br><span>8C MONZA BRIANZA</span></p></li>
+                <li><p class="caracteristicas">ENGINE SIZE<br><span>3200cc</span></p></li>
+                <li><p class="caracteristicas">VIN<br><span>132911</span></p></li>
+                <li><p class="caracteristicas">STOCK<br><span>82313</span></p></li>
               </ul>
             </div>
 
@@ -94,28 +92,8 @@
             
             <div class="col-xl-6 descripcion_auto">
               <h2>description</h2>
-              <h4>Hand built full Aluminum body</h4>
               <p>
-                <span>1952 Alfa Romeo 1900 C52 Disco Volante Fianchi Stretti.</span>
-              </p>
-
-              <p>
-                The only Fianchi Stretti built by Alfa Romeo is sitting at the Schlumpf
-                museum in France.
-              </p>
-
-              <p>
-                The Fianch Stretti uses the Carrozzeria Touring Superleggera space
-                frame technology. This was the car that essentially set that standard
-                for the race cars of that era. This example is powered by an Alfa
-                Romeo dual overhead cam 1600 motor, matted to a 4 speed manual
-                transmission.
-              </p>
-
-              <p>
-                Exact dimensions and materials as the real car with a
-                high level of craftsman ship. The is titled clean in Florida as an 1952
-                Alfa Romeo.
+                1933 Alfa Romeo 8C Monza Brianza. A recreation of the highest levels completed two decades ago commission by a very well known collector. Powered by a Bi Block 8C 3200cc straight 8 cylinder engine with supercharger, mated to an Alfa Romeo gearbox. Period tires and Rudge wheels, period friction shock absorbers, period diameter lights and Brooklands windshield. A hand crafted work of art identical to the original.
               </p>
 
               <div class="content_button">
@@ -132,9 +110,9 @@
                 <div class="col-lg-6 content_categoria">
                   <h3 class="subtitulo">engine</h3>
                   <ul>
-                    <li><p class="caracteristicas_small">ENGINE type<br><span>gasoline</span></p></li>
-                    <li><p class="caracteristicas_small">BODY SizE<br><span>1600</span></p></li>
-                    <li><p class="caracteristicas_small">Fuel Specification<br><span>92/93 Octane</span></p></li>
+                    <li><p class="caracteristicas_small">ENGINE type<br><span>-</span></p></li>
+                    <li><p class="caracteristicas_small">BODY SizE<br><span>3200cc</span></p></li>
+                    <li><p class="caracteristicas_small">Fuel Specification<br><span>-</span></p></li>
                   </ul>                  
                 </div>
 
@@ -142,8 +120,8 @@
                   <h3 class="subtitulo">body</h3>
                   <ul>
                     <li><p class="caracteristicas_small">body color<br><span>rosso</span></p></li>
-                    <li><p class="caracteristicas_small">BODY Style<br><span>ROADSTER</span></p></li>
-                    <li><p class="caracteristicas_small">doors<br><span>2</span></p></li>
+                    <li><p class="caracteristicas_small">BODY Style<br><span>-</span></p></li>
+                    <li><p class="caracteristicas_small">doors<br><span>-</span></p></li>
                     <li><p class="caracteristicas_small">paint type<br><span>-</span></p></li>
                   </ul>                  
                 </div>
@@ -155,21 +133,21 @@
                 <div class="col-lg-6 content_categoria">
                   <h3 class="subtitulo">basic</h3>
                   <ul>
-                    <li><p class="caracteristicas_small">year<br><span>1952</span></p></li>
+                    <li><p class="caracteristicas_small">year<br><span>1933</span></p></li>
                     <li><p class="caracteristicas_small">make<br><span>alfa romeo</span></p></li>
-                    <li><p class="caracteristicas_small">year<br><span>1900</span></p></li>
-                    <li><p class="caracteristicas_small">miles<br><span>10900</span></p></li>
+                    <li><p class="caracteristicas_small">model<br><span>8c monza</span></p></li>
+                    <li><p class="caracteristicas_small">miles<br><span>-</span></p></li>
                   </ul>                  
                 </div>
 
                 <div class="col-lg-6 content_categoria">
                   <h3 class="subtitulo">interior</h3>
                   <ul>
-                    <li><p class="caracteristicas_small">interior Color<br><span>Burgundy</span></p></li>
+                    <li><p class="caracteristicas_small">interior Color<br><span>Black</span></p></li>
                     <li><p class="caracteristicas_small">Secondary Interior Color<br><span>-</span></p></li>
-                    <li><p class="caracteristicas_small">Seating Type<br><span>Bucket</span></p></li>
-                    <li><p class="caracteristicas_small">Seat Material<br><span>Leather</span></p></li>
-                    <li><p class="caracteristicas_small">Shifter Type<br><span>Floor</span></p></li>
+                    <li><p class="caracteristicas_small">Seating Type<br><span>-</span></p></li>
+                    <li><p class="caracteristicas_small">Seat Material<br><span>-</span></p></li>
+                    <li><p class="caracteristicas_small">Shifter Type<br><span>-</span></p></li>
                     <li><p class="caracteristicas_small">Center Console<br><span>No</span></p></li>
                   </ul>                  
                 </div>
@@ -185,9 +163,6 @@
     </section>
     <!-- Available end -->
 
-    <!-- Slide Video Alfa 1900 -->
-    <?php include('includes/slide-alfa-1900.php'); ?>
-
     <!-- Galeria -->
     <section data-aos="fade-up" class="container-fluid galeria">
       <div class="row">
@@ -196,23 +171,23 @@
           <div class="row">
             
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-a.jpg" alt="galeria alfa romeo a">
+              <img class="img-fluid" src="./img/monza-a.jpg" alt="galeria alfa romeo monza a">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-b.jpg" alt="galeria alfa romeo b">
+              <img class="img-fluid" src="./img/monza-b.jpg" alt="galeria alfa romeo monza b">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-c.jpg" alt="galeria alfa romeo c">
+              <img class="img-fluid" src="./img/monza-c.jpg" alt="galeria alfa romeo monza c">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-d.jpg" alt="galeria alfa romeo d">
+              <img class="img-fluid" src="./img/monza-d.jpg" alt="galeria alfa romeo monza d">
             </div>
 
             <div class="col-md-12 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-e.jpg" alt="galeria alfa romeo e">
+              <img class="img-fluid" src="./img/monza-e.jpg" alt="galeria alfa romeo monza e">
             </div>
 
           </div>
