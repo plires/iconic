4c53e7be2a52f4b22889e11395c551e416c6b599
Pablo Lires <pablolires@gmail.com>
2023-03-08 16:40:40 -0300

    Edicion de todo el contenido de Fianchi_011

--- alfa-romeo-fianchi-stretti-011.php
+++ alfa-romeo-fianchi-stretti-011.php
@@ -31,7 +31,7 @@
   <?php include('includes/header.php'); ?>
 
   <!-- PAGE -->
-  <sction class="page_fianchi">
+  <sction class="page_fianchi page_fianchi_011">
 
     <!-- Slide -->
     <section class="container-fluid carousel-fade slides_home">
@@ -73,22 +73,22 @@
                 <div class="carousel-inner">
 
                   <div class="carousel-item active">
-                    <img src="./img/alfa-1900-slide-1.jpg" class="d-block w-100" alt="slide 1 alfa romeo">
+                    <img src="./img/fianchi-011-slide-1.jpg" class="d-block w-100" alt="slide 1 alfa romeo fianchi 011">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-2.jpg" class="d-block w-100" alt="slide 2 alfa romeo">
+                    <img src="./img/fianchi-011-slide-2.jpg" class="d-block w-100" alt="slide 2 alfa romeo fianchi 011">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-3.jpg" class="d-block w-100" alt="slide 3 alfa romeo">
+                    <img src="./img/fianchi-011-slide-3.jpg" class="d-block w-100" alt="slide 3 alfa romeo fianchi 011">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-4.jpg" class="d-block w-100" alt="slide 4 alfa romeo">
+                    <img src="./img/fianchi-011-slide-4.jpg" class="d-block w-100" alt="slide 4 alfa romeo fianchi 011">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-5.jpg" class="d-block w-100" alt="slide 5 alfa romeo">
+                    <img src="./img/fianchi-011-slide-5.jpg" class="d-block w-100" alt="slide 5 alfa romeo fianchi 011">
                   </div>
                   <div class="carousel-item">
-                    <img src="./img/alfa-1900-slide-6.jpg" class="d-block w-100" alt="slide 6 alfa romeo">
+                    <img src="./img/fianchi-011-slide-6.jpg" class="d-block w-100" alt="slide 6 alfa romeo fianchi 011">
                   </div>
 
                 </div>
@@ -106,13 +106,13 @@
             <div class="col-lg-6 data">
               <ul>
                 <li><h2 class="title">available</h2></li>
-                <li><p class="modelo">1952 alfa romeo<br><span>1900 c52</span></p></li>
+                <li><p class="modelo">1952 alfa romeo<br><span>DISCO VOLANTE FIANCHI STRETTI</span></p></li>
                 <li><p class="caracteristicas">BODY NUMBER<br><span>011</span></p></li>
-                <li><p class="caracteristicas">ENGINE SIZE<br><span>1600</span></p></li>
+                <li><p class="caracteristicas">ENGINE SIZE<br><span>2.5L</span></p></li>
                 <li><p class="caracteristicas">BODY STYLE<br><span>roadster</span></p></li>
-                <li><p class="caracteristicas">MILES<br><span>3,009</span></p></li>
-                <li><p class="caracteristicas">VIN<br><span>OHA005FS18</span></p></li>
-                <li><p class="caracteristicas">STOCK<br><span>FS18</span></p></li>
+                <li><p class="caracteristicas">MILES<br><span>-</span></p></li>
+                <li><p class="caracteristicas">VIN<br><span>AR119A0001175</span></p></li>
+                <li><p class="caracteristicas">STOCK<br><span>82403</span></p></li>
               </ul>
             </div>
 
@@ -122,28 +122,14 @@
             
             <div class="col-xl-6 descripcion_auto">
               <h2>description</h2>
-              <h4>Hand built full Aluminum body</h4>
+              <h4>1952 Alfa Romeo C52 Disco Volante Fianchi Stretti.</h4>
+
               <p>
-                <span>1952 Alfa Romeo 1900 C52 Disco Volante Fianchi Stretti.</span>
+                The only Fianchi Stretti built by Alfa Romeo is sitting at the Schlumpf museum in France. The cars we're reviewing is a recreation by Iconic Auto Sports in Argentina. A boutique vehicle manufacture dedicated to building exclusive coach work with hand made aluminum bodies of historic vintage cars. The company has three Autoclasica awards. Autoclasica is the most important concours in South America and ranked top 8 in the world. The building process of the cars are just like they were 80 years ago.
               </p>
 
               <p>
-                The only Fianchi Stretti built by Alfa Romeo is sitting at the Schlumpf
-                museum in France.
-              </p>
-
-              <p>
-                The Fianch Stretti uses the Carrozzeria Touring Superleggera space
-                frame technology. This was the car that essentially set that standard
-                for the race cars of that era. This example is powered by an Alfa
-                Romeo dual overhead cam 1600 motor, matted to a 4 speed manual
-                transmission.
-              </p>
-
-              <p>
-                Exact dimensions and materials as the real car with a
-                high level of craftsman ship. The is titled clean in Florida as an 1952
-                Alfa Romeo.
+                The Fianch Stretti uses the Carrozzeria Touring Superleggera space frame technology. This was the car that essentially set that standard for the race cars of that era. This example is powered by an Alfa Romeo V6 motor, matted to a 4 speed manual transmission.
               </p>
 
               <div class="content_button">
@@ -161,7 +147,7 @@
                   <h3 class="subtitulo">engine</h3>
                   <ul>
                     <li><p class="caracteristicas_small">ENGINE type<br><span>gasoline</span></p></li>
-                    <li><p class="caracteristicas_small">BODY SizE<br><span>1600</span></p></li>
+                    <li><p class="caracteristicas_small">BODY SizE<br><span>2.5L</span></p></li>
                     <li><p class="caracteristicas_small">Fuel Specification<br><span>92/93 Octane</span></p></li>
                   </ul>                  
                 </div>
@@ -170,7 +156,7 @@
                   <h3 class="subtitulo">body</h3>
                   <ul>
                     <li><p class="caracteristicas_small">body number<br><span>011</span></p></li>
-                    <li><p class="caracteristicas_small">body color<br><span>rosso</span></p></li>
+                    <li><p class="caracteristicas_small">body color<br><span>Red</span></p></li>
                     <li><p class="caracteristicas_small">BODY Style<br><span>ROADSTER</span></p></li>
                     <li><p class="caracteristicas_small">doors<br><span>2</span></p></li>
                     <li><p class="caracteristicas_small">paint type<br><span>-</span></p></li>
@@ -186,18 +172,18 @@
                   <ul>
                     <li><p class="caracteristicas_small">year<br><span>1952</span></p></li>
                     <li><p class="caracteristicas_small">make<br><span>alfa romeo</span></p></li>
-                    <li><p class="caracteristicas_small">year<br><span>1900</span></p></li>
-                    <li><p class="caracteristicas_small">miles<br><span>10900</span></p></li>
+                    <li><p class="caracteristicas_small">model<br><span>Disco Volante</span></p></li>
+                    <li><p class="caracteristicas_small">miles<br><span>-</span></p></li>
                   </ul>                  
                 </div>
 
                 <div class="col-lg-6 content_categoria">
                   <h3 class="subtitulo">interior</h3>
                   <ul>
-                    <li><p class="caracteristicas_small">interior Color<br><span>Burgundy</span></p></li>
+                    <li><p class="caracteristicas_small">interior Color<br><span>—</span></p></li>
                     <li><p class="caracteristicas_small">Secondary Interior Color<br><span>-</span></p></li>
                     <li><p class="caracteristicas_small">Seating Type<br><span>Bucket</span></p></li>
-                    <li><p class="caracteristicas_small">Seat Material<br><span>Leather</span></p></li>
+                    <li><p class="caracteristicas_small">Seat Material<br><span>-</span></p></li>
                     <li><p class="caracteristicas_small">Shifter Type<br><span>Floor</span></p></li>
                     <li><p class="caracteristicas_small">Center Console<br><span>No</span></p></li>
                   </ul>                  
@@ -214,8 +200,8 @@
     </section>
     <!-- Available end -->
 
-    <!-- Slide Video Alfa 1900 -->
-    <?php include('includes/video-alfa-1900.php'); ?>
+    <!-- Slide Video Fianchi 11 -->
+    <?php include('includes/video-fianchi-011.php'); ?>
 
     <!-- Galeria -->
     <section data-aos="fade-up" class="container-fluid galeria">
@@ -225,23 +211,23 @@
           <div class="row">
             
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-a.jpg" alt="galeria alfa romeo a">
+              <img class="img-fluid" src="./img/fianchi-011-slide-7.jpg" alt="galeria alfa romeo a">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-b.jpg" alt="galeria alfa romeo b">
+              <img class="img-fluid" src="./img/fianchi-011-slide-8.jpg" alt="galeria alfa romeo b">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-c.jpg" alt="galeria alfa romeo c">
+              <img class="img-fluid" src="./img/fianchi-011-slide-4.jpg" alt="galeria alfa romeo c">
             </div>
 
             <div class="col-md-6 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-d.jpg" alt="galeria alfa romeo d">
+              <img class="img-fluid" src="./img/fianchi-011-slide-2.jpg" alt="galeria alfa romeo d">
             </div>
 
             <div class="col-md-12 img_galeria">
-              <img class="img-fluid" src="./img/alfa-1900-e.jpg" alt="galeria alfa romeo e">
+              <img class="img-fluid" src="./img/fianchi-011-large.jpg" alt="galeria alfa romeo e">
             </div>
 
           </div>
---
+++ includes/video-fianchi-011.php
@@ -0,0 +1,13 @@
+<section data-aos="fade-up" class="container-fluid p-0 video">
+  <div class="row">
+
+    <div class="col-md-12">
+      <div class="ratio ratio-16x9">
+        <iframe class='embed-responsive-item' src='https://www.youtube.com/embed/3KPEOJVfGHQ?rel=0&enablejsapi=1&mute=1&loop=1&autoplay=1&' frameborder='0' ></iframe>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+
